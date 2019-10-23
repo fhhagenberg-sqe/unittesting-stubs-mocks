426880ff4f0f7292eca37a82402cc7f8c155010e
Mario Winterer <mario.winterer@scch.at>
2019-10-23 18:08:49 +0200

    add initial content

---
+++ src/test/java/at/fhhagenberg/sqe/stubsmocks/SimpleTest.java
@@ -0,0 +1,10 @@
+package at.fhhagenberg.sqe.stubsmocks;
+
+import org.junit.jupiter.api.Test;
+
+public class SimpleTest {
+    @Test
+    public void testNothing() {
+        /* nothing to do */
+    }
+}
